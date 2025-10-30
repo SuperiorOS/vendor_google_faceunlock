8d663cf1e8469cd0465051e465041de8c122875b
anayw2001 <anay1018@gmail.com>
2025-10-30 16:27:02 +0530

    faceunlock: Add Settings src overlay

--- config.mk
+++ config.mk
@@ -16,7 +16,8 @@ DEVICE_PACKAGE_OVERLAYS += vendor/google/faceunlock/overlay
 
 # Runtime resource overlays
 PRODUCT_PACKAGES += \
-    FaceEnrollOverlay
+    FaceEnrollOverlay \
+    FaceEnrollSettingsOverlay
 
 # Permissions
 PRODUCT_COPY_FILES += \
